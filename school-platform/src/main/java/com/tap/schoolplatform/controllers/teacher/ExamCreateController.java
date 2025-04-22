dbd70bf53d4c383f6017e716101bb147427cb51f
Brisa <brisabcp@gmail.com>
2025-04-21 22:27:49 -0700

    Saving changes

--- school-platform/src/main/java/com/tap/schoolplatform/controllers/teacher/ExamCreateController.java
+++
@@ -1,4 +0,0 @@
-package com.tap.schoolplatform.controllers.teacher;
-
-public class ExamCreateController {
-}
---
+++ school-platform/src/main/java/com/tap/schoolplatform/controllers/teacher/TeacherViewController.java
@@ -0,0 +1,20 @@
+package com.tap.schoolplatform.controllers.teacher;
+
+import com.tap.schoolplatform.controllers.ViewController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class TeacherViewController extends ViewController {
+
+    @FXML public Label teacherName;
+    @FXML public Button logOutButton;
+    @FXML public BorderPane borderPane;
+
+    @FXML
+    public void logOut(){
+
+    }
+
+}
